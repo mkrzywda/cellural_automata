9dae89f539ea2097279f7dc81568fd5690f6fdc1
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2019-10-28 17:51:40 +0100

    Change name of var

--- rules/automata_1d.hs
+++ rules/automata_1d.hs
@@ -5,15 +5,15 @@ import Data.Array (listArray, (!), bounds, elems)
 --http://hackage.haskell.org/package/array-0.5.4.0/docs/Data-Array.html
 --
 
-step rule a = listArray (l,r) res
+step rules a = listArray (l,r) res
   where (l,r) = bounds a
-        res = [rule (a!r)     (a!l) (a!(l+1)) ] ++
-              [rule (a!(i-1)) (a!i) (a!(i+1)) | i <- [l+1..r-1] ] ++
-              [rule (a!(r-1)) (a!r) (a!l)     ]
+        res = [rules (a!r)     (a!l) (a!(l+1)) ] ++
+              [rules (a!(i-1)) (a!i) (a!(i+1)) | i <- [l+1..r-1] ] ++
+              [rules (a!(r-1)) (a!r) (a!l)     ]
  
-runCA rule = iterate (step rule)
+runCA rules = iterate (step rules)
 
-rule n l x r = n `div` (2^(4*l + 2*x + r)) `mod` 2
+rules n l x r = n `div` (2^(4*l + 2*x + r)) `mod` 2
 
 initial n = listArray (0,n-1) . center . padRight n
   where
@@ -24,7 +24,7 @@ initial n = listArray (0,n-1) . center . padRight n
 -- fmap :: (a -> b) -> f a -> f b
 
 
-display1DCA n rule init = mapM_ putStrLn $ take n result
-  where result = fmap display . elems <$> runCA rule init
+display1DCA n rules init = mapM_ putStrLn $ take n result
+  where result = fmap display . elems <$> runCA rules init
         display 0 = ' '
         display 1 = '#'
